c1fe25600bbc874be04c9d973f4c520187d814e6
nicolaslarrondo <nicolaslarrondo@liceovvh.cl>
2024-09-24 08:15:14 -0300

    hola

---
+++ hola2/javascitp.js
@@ -0,0 +1,9 @@
+
+
+
+
+    function eliminar() {
+        const elemento = document.getElementById("elemento-ocultar");
+        elemento.remove(); // Añade la clase para ocultarlo
+        alert("this page says nija was liked")
+    }
