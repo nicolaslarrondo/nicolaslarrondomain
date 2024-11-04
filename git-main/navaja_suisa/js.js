ef44587ade14b6381504e997a2e11a8760fabfb5
nicolaslarrondo <nicolaslarrondo@liceovvh.cl>
2024-11-04 14:14:50 -0300

    ok

--- git-main/navaja_suisa/js.js
+++ git-main/navaja_suisa/js.js
@@ -1,52 +1,78 @@
 
 
-/*function boton_1(){
+function boton_1(){
 let nombre = prompt("ingrese su nombre");
 let apellido = prompt("ingrese su apellido");
 let edad = parseInt(prompt("ingrese su edad"));
 
-alert("bienvenido señor "+nombre+" "+apellido+" y su edad es "+edad)
+edad == edad?alert(`bienvenido señor ${nombre} ${apellido} y su ${edad} es edad`):alert(`${edad} no es un numero y porende no una edad`)
+
 }
-boton_1()*/
 
-/*function boton_2(){
 
+function boton_2(){
+let pl
 var numero = parseInt(prompt("ingrese un numero"))
+numero == numero?pl = numero / 3 + 10 * 2:alert("hola").return
 
-let pl = numero / 3 + 10 * 2
-console.log(pl);
+alert(pl);
 
 }
-boton_2();*/
 
-/*function boton_3(){
-    let numero = parseInt(prompt("ingrese un numero"))
-    let numero2 = parseInt(prompt("ingrese un numero"))
-    
-    numero > numero2? alert(numero+""+numero2):alert("no es de mayor a menor")
+
+function boton_3(){
+    let numero = parseInt(prompt("ingrese un numero")),numero2 = parseInt(prompt("ingrese un numero")) 
+    numero == numero || numero2 == numero2?numero > numero2?
+    alert(`${numero} es mayor que ${numero2}`)
+    :alert(`${numero2} es mayor que ${numero}`).return:
+    alert("hola")
+   
 }
-boton_3()*/
 
-/*function boton_4(){
+
+function boton_4(){
     let re = 0
 for (let i = 0; i < 5 ; i++) {
     let numero = parseInt(prompt("ingrese un numero"))
-   re += numero
+    numero == numero?re += numero: alert(`${numero} no es un numero, vuelve a precionar el boton 4 y ingrese un numero`).return
 }
 alert(re = re/6)
 }
-boton_4()*/
 
-/*function boton_5(){
-    let ve = parseInt(prompt()),o = 0
-    for (let i = 0; i < ve ; i++) {
-       let numero = parseInt(prompt("hola"))
-        o += numero
+
+function boton_5(){
+    let ve = parseInt(prompt("por favor ingrese un numero")),o =0
+    for (let i = 0; i < ve; i++) {
+        o += parseInt(prompt("por favor ingrese un numero"))   
     }
-    alert(o)
+alert(o)
 }
-boton_5()*/
 
 function boton_6(){
-    
+   let par = [],o = 1
+
+    for(let i = 1;i < 166;i++){     
+    o = i % 2
+     if(o == 0 ){
+      par.push(i)
+     }
+    }
+alert(par)
 }
+
+function boton_7(){
+    let par = [],o = 1
+
+    for(let i = 1;i < 166;i++){     
+    o = i % 2
+     if(o != 0 ){
+      par.push(i)
+     }
+    }
+alert(par)
+}
+
+function boton_8(){
+
+}
+
--- hola_mundo/ppp.js
+++ hola_mundo/ppp.js
@@ -7,22 +7,22 @@ var h3 = document.querySelector("h1");
 h3.innerText = "new title";
 
 var petImg = document.querySelector("#pet-img");
-function switchImg() {
+function switchimg() {
 petImg.src = "dog.jog";
 }
 
-function setActive(element) {
+function switchimg(element) {
     element.style.backgroundColor = "#222222";
     element.style.color = "#ffffff";
 }
 
-function setActive(element) {
-    if(element.classList.includes("dark-mpode")) {
-    element.innerText = "cambio al modo claro";
+function setactive(element) {
+    if(element.classList.contains("dark-mode")) {
+    element.innerText = "cambiar al modo claro";
     element.classList.remove("dark-mode");
     } else {
     element.innerText = "cambiar al modo oscuro"
-    oscuro&quot;;
+   
     element.classList.add("dark-mode");
     }
-    }
+}
